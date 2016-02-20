48dac2749e12fa67319941a1d6d0df8960cfd18d
Alonbt <alonbt@gmail.com>
2016-02-20 04:43:12 +0200

    setting up environment

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularAnimatorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularAnimatorApp
+ */
+angular.module('angularAnimatorApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularAnimatorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularAnimatorApp
+ */
+angular.module('angularAnimatorApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
